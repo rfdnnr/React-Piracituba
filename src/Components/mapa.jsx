b6635b8bd875bc51fc95b63cfc1e1ad6b8836fd9
Rafael <rafabeto@icloud.com>
2023-11-20 12:13:31 -0300

    mapa com marcadores

--- src/Components/mapa.jsx
+++ src/Components/mapa.jsx
@@ -8,16 +8,43 @@ const position = [-22.9083, -43.1964]
         
 const Mapa = () => (
 
-<MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ width: '100%', height: '400px' }}>
+<MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ width: '50%', height: '400px', margin: 'auto' }}>
 <TileLayer
   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 />
-<Marker position={[51.505, -0.09]}>
+<Marker position={[-22.9083, -43.1964]}>
   <Popup>
-    A pretty CSS3 popup. <br /> Easily customizable.
+    Código: 36
+<br />
+Nome: Centro
+<br />
+Horário: 23:15 18/08/2023
+<br />
+Chuva (últ 15m): 20.0 mm
+<br />
+Temperatura: 28°C
+<br />
+Vento: 10 m/s (NE) <br /> 
   </Popup>
 </Marker>
+
+<Marker position={[-22.9810, -43.1964]}>
+  <Popup>
+    Código: 37
+<br />
+Nome: Copacabana
+<br />
+Horário: 23:15 18/08/2023
+<br />
+Chuva (últ 15m): 18.0 mm
+<br />
+Temperatura: 26°C
+<br />
+Vento: 11 m/s (NE) <br /> 
+  </Popup>
+</Marker>
+
 </MapContainer>
 )
 
